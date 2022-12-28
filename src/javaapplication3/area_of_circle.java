4345d81cf8cc38a1919d54fc0ddf86488997ed1e
jassreet <95341734+jassreet@users.noreply.github.com>
2022-12-28 09:56:20 +0530

    area of circle

---
+++ src/javaapplication3/area_of_circle.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author DELL
+ */
+public class area_of_circle {
+    public static void main(String args[]){
+        int r=20;
+        float pi=3.141f;
+        float c=pi*r*r;
+        System.out.print("area of circle is"+c);
+    }
+}
