c076b3c6252db189890c5fc15cb69196f8a56033
jassreet <95341734+jassreet@users.noreply.github.com>
2022-12-28 10:52:06 +0530

    swap with variable

---
+++ src/javaapplication3/Swap_two_numbers_with_c.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author DELL
+ */
+public class Swap_two_numbers_with_c {
+    public static void main(String args[]){
+        int a=10;
+        int b=20;
+        int c=a;
+         a=b;
+         b=c;
+         System.out.println("Value of a after swap"+a);
+         System.out.println("Value of b after swap"+b);
+        
+    }
+}
