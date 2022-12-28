266da04c3111294a849cabe7834d7ef2b24caa00
jassreet <95341734+jassreet@users.noreply.github.com>
2022-12-28 09:13:35 +0530

    Initial commit

---
+++ src/javaapplication3/NewClass.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author DELL
+ */
+public class NewClass {
+    public static void main(String args[] ){
+        for (int i = 0; i<6;i++)
+        {
+            for(int j = 0; j<i;j++)
+                System.out.print("*");
+            
+            System.out.println("");
+        }
+                
+    }
+    
+}
