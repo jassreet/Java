ca5c237ac610794278b4b93a1e8feec323380b04
jassreet <95341734+jassreet@users.noreply.github.com>
2022-12-28 10:29:36 +0530

    logical operator

---
+++ src/javaapplication3/logical.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author DELL
+ */
+public class logical {
+    public static void main(String args[]){
+        int a=55;
+        int b=70;
+        if(a<50 && a<b)
+        {
+            System.out.print("Condition Satisfied");
+        }
+        else{
+        System.out.print("Condition Not Satisfied");
+    }
+    }
+}
