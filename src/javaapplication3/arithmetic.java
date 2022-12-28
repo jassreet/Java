17f3e830f25de3911f75c8bdf387b98b7e0cdc7e
jassreet <95341734+jassreet@users.noreply.github.com>
2022-12-28 10:19:44 +0530

    arithmetic

---
+++ src/javaapplication3/arithmetic.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author DELL
+ */
+public class arithmetic {
+    public static void main(String args[]){
+        int a=2345;
+        int b=8;
+        int c=a+b;
+        float d=c/3;
+        float m=d%5;
+        float f=m*5;
+        System.out.print("Final result is"+f);
+    }
+}
