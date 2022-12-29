d4bbe91a9dc39bf8929a51285a5baa06ef5736b0
jassreet <95341734+jassreet@users.noreply.github.com>
2022-12-29 09:47:29 +0530

    k pattern star

---
+++ src/javaapplication3/k_pattern.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author DELL
+ */
+public class k_pattern {
+    public static void main(String args[]){
+         for (int i=5;i>0;i--)
+        {
+            for(int j=0;j<i;j++)
+                System.out.print("*");
+            
+            System.out.println("");
+        }
+         for (int i = 2; i<6;i++)
+        {
+            for(int j = 0; j<i;j++)
+                System.out.print("*");
+            
+            System.out.println("");
+        }
+    }
+}
