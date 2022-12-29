d1010e09e969bdaea2673adc754919a6c631fc9d
jassreet <95341734+jassreet@users.noreply.github.com>
2022-12-29 09:40:32 +0530

    space pattern
    put space with j

--- src/javaapplication3/space.java
+++ src/javaapplication3/space.java
@@ -11,13 +11,16 @@ package javaapplication3;
  */
 public class space {
     public static void main(String args[]){
-         for (int i = 0; i<8;i++)
+         for (int i = 5; i>0;i--)
         {
             for(int j = 0; j<i;j++)
-                System.out.print("*");
+            {System.out.print(" ");}
             
+            for(int k=i;k<5;k++)
+            {System.out.print("*");}
             System.out.println("");
+        }
         }
     }
     
-}
+
