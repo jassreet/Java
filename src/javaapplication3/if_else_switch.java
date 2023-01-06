9a54b1bd9c64aeae8f13e04e92d349d376a35d4b
jassreet <95341734+jassreet@users.noreply.github.com>
2023-01-06 15:36:42 +0530

    if else switch

--- src/javaapplication3/if_else_switch.java
+++ src/javaapplication3/if_else_switch.java
@@ -13,37 +13,37 @@ import java.util.Scanner;
  */
 public class if_else_switch {
     public static void main(String args[]){
-    Scanner sc=new Scanner(System.in);
-        System.out.println("enter the value of a");
-        int a=sc.nextInt();
-        System.out.println("enter the value of b");
-        int b=sc.nextInt();
-        System.out.println("enter the value of c\n1 for addition\n2for subtraction\n3for division\n4for multiplication\n5for remainder");
-        int c=sc.nextInt();
-        switch(c) {
+        int a;
+        int b;
+        int c;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of a" );
+        a = sc.nextInt();
+        System.out.println("Enter the value of b");
+        b = sc.nextInt();
+        System.out.println("Select the operation to perform \n1 for addition \n2 for subtraction \n3 for division \n4 for multiplication \n5 for modolus");
+        c = sc.nextInt();
+        if(c<1 || c>5){
+            System.out.println("Invalid Input");
+        }
+        switch(c){
             case 1:
-                System.out.println("ans is "+(a+b));
-            
-            break;
+                System.out.println("Sum is" +(a+b));
+                break;
             case 2:
-                System.out.println("ans is "+(a-b));
-            
-            break;
+                System.out.println("Subtraction is" +(a-b));
+                break;
             case 3:
-                System.out.println("ans is "+(a/b));
-            
-            break;
+                System.out.println("Division is" +(a/b));
+                break;
             case 4:
-                System.out.println("ans is "+(a*b));
-            
-            break;
+                System.out.println("Multilication is" +(a*b));
+                break;
             case 5:
-                System.out.println("ans is "+(a%b));
-            break;
-            default:
-            System.out.println("not valid");
-            break;
+                System.out.println("Modolus is" +(a%b));
+                break;
+                
         }
-        }
+    }
 
 }
